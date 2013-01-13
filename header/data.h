64c4c437b5edf1402f464f9c14d3677ece34417a
ikerexxe <pedrosalonso@hotmail.com>
2013-01-13 13:06:34 +0100

    Commit inicial en el que se añade la estructura de datos y los protolos de comunicacion

---
+++ header/data.h
@@ -0,0 +1,135 @@
+/*****************************************************************************
+**											  								**
+** project :  BroadCar						 							  	**
+** filename : data.h 							  					    	**
+** version : 1 									  							**
+** date :  	2013-01-13		 		 									    **
+** 											  								**
+******************************************************************************
+** 											  								**
+**                      											  		**
+**  								  					                    **
+** 											  								**
+******************************************************************************
+**										        							**
+**VERSION HISTORY:									  						**
+**----------------									  						**
+**Version : 1										  						**
+**Date : 2013-01-13									 					    **
+**Revised by : 			                    	  							**
+**Description : Original version.							  				**
+*****************************************************************************/
+/*****************************************************************************
+** 																			**
+** DEFINITIONS AND MACROS 													**
+** 																			**
+******************************************************************************/
+#ifndef DATA_H
+#define DATA_H
+
+//Listas numeradas
+typedef enum tipo_sensor{LUMINOSIDAD, LLUVIA, LIQUIDO_CARRETERA, TEMPERATURA, VELOCIDAD};
+typedef enum hemisferio{NORTE, SUR, ESTE, OESTE};
+typedef enum tipo_mensaje{TRAFICO_DENSO, OBRAS, VEHICULO_NO_VISIBLE, POCA_VISIBILIDAD, ESTADO_CARRETERA, ACCIDENTE_CARRETERA};
+typedef enum tipo_poca_visibilidad{NIEVE, LLUVIA, NIEBLA};
+typedef enum tipo_estado_carretera{HIELO, ACEITE, BARRO, AGUA, DETERIORO};
+
+/*
+ * Estructura propia existente en trafico denso
+ */
+typedef struct s_TRAFICO_DENSOCls{
+	int direccion;			//(0=Km carretera de menor a mayor / 1=Km de mayor a menor )
+	int velocidad;
+}TRAFICO_DENSOClass;
+
+/*
+ * Estructura propia existente en obras
+ */
+typedef struct s_OBRASCls{
+	int direccion;			//(0=Km carretera de menor a mayor / 1=Km de mayor a menor )
+	int carretera_cortada;
+}OBRASClass;
+
+/*
+ * Estructura propia existente en vehiculo no visible
+ */
+typedef struct s_VEHICULO_NO_VISIBLECls{
+	int direccion;			//(0=Km carretera de menor a mayor / 1=Km de mayor a menor )
+	int velocidad;
+}VEHICULO_NO_VISIBLEClass;
+
+/*
+ * Estructura propia existente en poca visibilidad
+ */
+typedef struct s_POCA_VISIBILIDADCls{
+	enum tipo_poca_visibilidad tipo;
+	int gravedad;
+}POCA_VISIBILIDADClass;
+
+/*
+ * Estructura propia existente en estado carretera
+ */
+typedef struct s_ESTADO_CARRETERACls{
+	enum tipo_estado_carretera tipo;
+	int direccion;			//(0=Km carretera de menor a mayor / 1=Km de mayor a menor )
+	int gravedad;
+}ESTADO_CARRETERAClass;
+
+/*
+ * Estructura propia existente en accidente carretera
+ */
+typedef struct s_ACCIDENTE_CARRETERACls{
+	int direccion;			//(0=Km carretera de menor a mayor / 1=Km de mayor a menor )
+	int carretera_cortada;
+}ACCIDENTE_CARRETERAClass;
+
+/*
+ * Estructura que guarda las coordenadas
+ */
+typedef struct s_COORDENADASCls{
+	enum hemisferio latitud;
+	int latitud_grado;
+	int latitud_minuto;
+	int latitud_segundo;
+	enum hemisferio longitud;
+	int longitud_grado;
+	int longitud_minuto;
+	int longitud_segundo;
+}COORDENADASClass;
+
+/*
+ * Estructura que guarda los datos de un sensor
+ */
+typedef struct s_SENSORCls{
+	enum tipo_sensor tipo;
+	int hora;				//TODO: el tipo se podria cambiar en un futuro
+	COORDENADASClass posicion;
+	float valor;
+}SENSORClass;
+
+/*
+ * Estructura que guarda los mensajes recibidos de otros controladores
+ */
+typedef struct s_MENSAJECls{
+	enum tipo_mensaje tipo;
+	int id;
+	int hora;				//TODO: el tipo se podria cambiar en un futuro
+	COORDENADASClass posicion;
+	int ttl;				//numero de veces que se puede reenviar
+	void * valor;
+}MENSAJEClass;
+
+/*
+ * Estructura que guarda los datos a enviar a la interfaz grafica
+ */
+typedef struct s_INTERFAZ_GRAFICACls{
+	enum tipo_mensaje tipo;
+	COORDENADASClass posicion;
+	void * valor;
+}INTERFAZ_GRAFICAClass;
+/*****************************************************************************
+** 																			**
+** EOF 																		**
+** 																			**
+******************************************************************************/
+#endif
