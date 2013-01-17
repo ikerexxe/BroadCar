0f4ab715ea34f31920c38d440e465b1d366fdb21
ikerexxe <pedrosalonso@hotmail.com>
2013-01-17 15:30:03 +0100

    se llee el estado del teclado

--- main.c
+++ main.c
@@ -1,8 +1,112 @@
+/*********************************************************************
+** 																	**
+** project : BroadCar		 										**
+** filename : main.c 												**
+** version : 1 														**
+** date : 2013-01-17	 											**
+** 																	**
+**********************************************************************
+** 																	**
+** Copyright (c) 2012,		 										**
+** All rights reserved. 											**
+** 																	**
+**********************************************************************
+**																	**
+**VERSION HISTORY:													**
+**----------------													**
+**Version : 1														**
+**Date : 2013-01-17													**
+**Revised by : iker pedrosa											**
+**Description : Original version.									**
+*********************************************************************/
 
+/*********************************************************************
+**																	**
+** MODULES USED 													**
+** 																	**
+**********************************************************************/
+#include "framebuffer.h"
+#include "clock.h"
+#include "displayGenerico.h"
+#include "display.h"
+#include "hw_types.h"
+
+/*********************************************************************
+** 																	**
+** PROTOTYPES OF LOCAL FUNCTIONS 									**
+** 																	**
+*********************************************************************/
+void CHAT_inicializacion();
+void CHAT_logica();
+
+//TODO: esto va en el header
+#define MAX_ELEMS_LINEA 10
+
+/*********************************************************************
+** 																	**
+** GLOBAL VARIABLES 												**
+** 																	**
+**********************************************************************/
+unsigned long g_ul_system_clock; /*Frecuencia del clock*/
+unsigned char uno = 97;
+unsigned char dos = 98;
+
+/*********************************************************************
+** 																	**
+** LOCAL FUNCTIONS 													**
+** 																	**
+**********************************************************************/
 int main(void)
 {
+	int i = 0;
 
-    while(1)
-    {
+	CHAT_inicializacion();
+
+    while(1){
+    	CHAT_logica();
     }
 }
+
+/**
+ * @brief  Función para inicialización de los periféricos.
+ *
+ * @return    -
+ *
+ * Se inicializan uno a uno el clock, el keypad, la pantalla, el PWM y
+ * la UART.
+*/
+void CHAT_inicializacion(){
+	CHAT_inicializacion_clock();
+//	CHAT_inicializacion_keypad();
+	CHAT_inicializacion_display();
+//	CHAT_inicializacion_pwm();
+//	CHAT_inicializacion_comunicacion();
+}
+
+/**
+ * @brief  Función para elegir el paso a seguir en cada momento.
+ *
+ * @return    -
+ *
+ * Primero se lee la tecla pulsada, luego se actualiza la pantalla teniendo
+ * en cuenta la tecla pulsa, si se pulsa el select se envía el mensaje y se
+ * reinicializa la parte de la pantalla en la que escribe el usuario.
+ * Si se ha reibido algo por la UART y está almacenado en el buffer de software
+ * se muestra en pantalla.
+*/
+void CHAT_logica(){
+	unsigned char * pantalla = malloc(sizeof(unsigned char) * MAX_ELEMS_LINEA);
+
+	pantalla[0] = uno;
+	pantalla[1] = dos;
+	pantalla[2] = '\0';
+	uno++;
+	dos++;
+
+	CHAT_escribir(pantalla);
+}
+/*********************************************************************
+** 																	**
+** EOF 																**
+** 																	**
+**********************************************************************/
---
+++ src/display.c
@@ -0,0 +1,146 @@
+/*********************************************************************
+** 																	**
+** project : BroadCar		 										**
+** filename : display.c												**
+** version : 1 														**
+** date : 2012-10-08	 											**
+** 																	**
+**********************************************************************
+** 																	**
+** Copyright (c) 2012,		 										**
+** All rights reserved. 											**
+** 																	**
+**********************************************************************
+**																	**
+**VERSION HISTORY:													**
+**----------------													**
+**Version : 3														**
+**Date : 2013-01-17													**
+**Revised by : iker pedrosa											**
+**Description : Modified version for broadcar application.			**
+*********************************************************************/
+
+/*********************************************************************
+**																	**
+** MODULES USED 													**
+** 																	**
+**********************************************************************/
+#include "rit128x96x4.h"
+#include "display.h"
+#include "displayGenerico.h"
+#include "framebuffer.h"
+#include "hw_types.h"
+#include <stdlib.h>
+/*********************************************************************
+** 																	**
+** DEFINITIONS AND MACROS 											**
+** 																	**
+**********************************************************************/
+#define DISPLAY_C
+
+/*********************************************************************
+** 																	**
+** EXPORTED VARIABLES 												**
+** 																	**
+*********************************************************************/
+extern unsigned long g_ul_keypad_switches; /*Valor leído en los botones*/
+extern unsigned char g_ucChangedData; /*Si ha cambiado la tecla que se está pulsando*/
+
+/*********************************************************************
+** 																	**
+** GLOBAL VARIABLES 												**
+** 																	**
+**********************************************************************/
+int g_i_numero_elemento = 0; /*Identificador del ultimo texto introducido en pantalla*/
+int g_i_altura_conversacion = 10; /*Altura en la cual se encuentra la conversacion*/
+
+const unsigned char g_puc_circ[60]  =  {
+        0x00, 0x00, 0x44, 0x44, 0x00, 0x00,
+        0x00, 0x4a, 0xaa, 0xaa, 0xa4, 0x00,
+        0x04, 0xaa, 0xaa, 0xaa, 0xaa, 0x40,
+        0x4a, 0xaa, 0xaa, 0xaa, 0xaa, 0xa4,
+        0x4a, 0xaa, 0xaa, 0xaa, 0xaa, 0xa4,
+        0x4a, 0xaa, 0xaa, 0xaa, 0xaa, 0xa4,
+        0x4a, 0xaa, 0xaa, 0xaa, 0xaa, 0xa4,
+        0x04, 0xaa, 0xaa, 0xaa, 0xaa, 0x40,
+        0x00, 0x4a, 0xaa, 0xaa, 0xa4, 0x00,
+        0x00, 0x00, 0x44, 0x44, 0x00, 0x00
+    }; /*Dibujo de un círculo*/
+const unsigned char g_puc_nada[60]  =  {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    }; /*Dibujo vacío del tamaño del círculo*/
+
+/*********************************************************************
+** 																	**
+** PROTOTYPES OF LOCAL FUNCTIONS 									**
+** 																	**
+*********************************************************************/
+//TODO: no hay
+
+/*********************************************************************
+** 																	**
+** LOCAL FUNCTIONS 													**
+** 																	**
+**********************************************************************/
+
+/**
+ * @brief  Inicializamos la pantalla.
+ *
+ * @return    -
+ *
+ * Primero se inicializa el buffer y luego se inicializa el texto del usuario.
+ * Al final, se vuelca a la pantalla real.
+*/
+void CHAT_inicializacion_display(){
+	unsigned char * str = malloc(sizeof(unsigned char) * 10);
+	unsigned char * str1 = malloc(sizeof(unsigned char) * 10);
+
+	FRAME_BUFFER_init();
+	strcpy(str, "inicia");
+	FRAME_BUFFER_insert_text(str, 0, g_i_altura_conversacion); //Escribimos en el buffer
+	g_i_altura_conversacion += 10;
+	g_i_numero_elemento++;
+	FRAME_BUFFER_write_to_display();
+}
+
+/**
+ * @brief  Insertar la ultima frase de la conversacion
+ *
+ * @return    -
+ *
+ * Si se recibe un texto se muestra en pantalla poniendo como inicio PC-> y si
+ * se envia se pone Yo->
+*/
+void CHAT_escribir(unsigned char * mensaje){
+	int contador = 0;
+	int altura = 10;
+
+	if(g_i_numero_elemento < MAX_ELEMS_PANTALLA){
+		g_i_numero_elemento = FRAME_BUFFER_insert_text(mensaje, 0, g_i_altura_conversacion);
+		g_i_altura_conversacion += 10;
+	} else{
+		contador = 0;
+		g_i_numero_elemento = FRAME_BUFFER_delete_element(contador);
+		for(contador; contador < g_i_numero_elemento; contador++){
+			FRAME_BUFFER_actualiza_posicion_elemento(contador, 0, altura);
+			altura += 10;
+		}
+		FRAME_BUFFER_insert_text(mensaje, 0, altura);
+	}
+
+	FRAME_BUFFER_write_to_display();
+}
+/*********************************************************************
+** 																	**
+** EOF 																**
+** 																	**
+**********************************************************************/
